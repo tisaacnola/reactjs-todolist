f196a8da67db427d683fc60811e56771cb3a5112
Odell Isaac <odellisaac@Odells-MacBook-Pro.local>
2024-09-25 18:59:28 -0500

    todo-app

---
+++ src/components/TodoCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function TodoCard(props) {
+    const {children, handleDeleteTodos, index, handleEditTodos} = props;
+  return (
+    <li className='todoItem'>
+        {children}
+        <div className='actionContainer'>
+            <button onClick={() =>{
+                handleEditTodos(index)
+            }}>
+                <i className="fa-solid fa-pen-to-square"></i>
+            </button>
+            <button onClick={() => {
+                handleDeleteTodos(index)
+            }}>
+                <i className="fa-solid fa-trash"></i>
+            </button>
+        </div>
+    </li>
+  )
+}
